e5322395e4d10fa9f02171cff3c44e235c49d9bf
Jason <jasoncdrea@gmail.com>
2025-09-04 13:48:05 +0200

    [UPDATE] Get game state

--- agent_client.py
+++ agent_client.py
@@ -1,38 +1,30 @@
-import socket
+import json, socket
 
 def main():
     host = 'localhost'
     port = 5000
-
     try:
-        # Create a TCP socket
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            # Connect to the Java server
             s.connect((host, port))
             print("Connected to server")
-
-            # Passive loop waiting for messages from the server
+            f = s.makefile('r', encoding='utf-8')
             while True:
-                try:
-                    # Receive data from the server
-                    data = s.recv(4096)
-                    if not data:
-                        print("Disconnected from server")
+                for line in f:
+                    line = line.strip()
+                    if not line:
+                        continue
+                    jsonObj = json.loads(line)
+                    n_nb_self = jsonObj['npc']['pokemonNb']
+                    p_nb_self = jsonObj['player']['pokemonNb']
+                    print(n_nb_self, p_nb_self)
+                    print("Data received :", jsonObj)
+                    if n_nb_self == 0 or p_nb_self == 0:
+                        print("npc or player defeated")
                         break
-                    print("Data received :", data.decode('utf-8'))
-
-                except ConnectionResetError:
-                    # Connection was forcibly closed by the Java server
-                    print("Connection reset by server")
-                    break
-
     except ConnectionRefusedError:
-        # Server is not running or unavailable
         print("Impossible to connect to server : Server unavailable")
     except KeyboardInterrupt:
-        # Graceful shutdown when Ctrl+C is pressed
         print("Closing python client")
-
 
 if __name__ == "__main__":
     main()
